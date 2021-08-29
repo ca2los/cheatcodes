d0691eef0d2b74f3fff7f13b1cd323f142519818
ca2los <{ID}+{username}@users.noreply.github.com>
2021-08-29 14:44:18 -0500

    Classes and Subclasses for JS.

--- module-10/index_09/index.js
+++ module-10/index_09/index.js
@@ -1,12 +1,13 @@
     // 09. CLASSES: Main program
 
     class GhostOfTsushima {
-        constructor(name,type,health,weapon,defense) {
+        constructor(name,type,health,weapon,defense,hit) {
             this.name = name;
             this.type = type;
             this.health = health;
             this.weapon = weapon;
             this.defense = defense;
+            this.hit = hit;
         }
 
         stats() {
@@ -18,10 +19,10 @@
 
     module.exports = GhostOfTsushima;
 
-    // @ Line 3-16
+    // @ Line 3-17
     // We have the main 'class declaration' to determine the core of the program.
     // The 'constructor' operator has arguments inside of it, promises all of them.
     // The 'stats' function is the main trigger of the program's 'class'.
 
-    // @ Line 18
+    // @ Line 19
     // Exports the class declaration into any program who calls it back.
--- module-10/index_09/trigger.js
+++ module-10/index_09/trigger.js
@@ -3,23 +3,25 @@
     const GhostOfTsushima = require('./index');
 
     class Hero extends GhostOfTsushima {
-        constructor(hero,samurai,life,katana,armor) {
+        constructor(hero,samurai,life,katana,armor,punch) {
             const name = hero + ' from Japan';
             const type = samurai + ' warrior';
             const health = life + ' hit points';
             const weapon = katana + ' sword';
             const defense = armor + ' from the main battle';
+            const hit = punch + ' attack';
 
-            super(name,type,health,weapon,defense);
+            super(name,type,health,weapon,defense,hit);
 
             this.hero = hero;
             this.samurai = samurai;
             this.life = life;
-            this.defense = katana;
+            this.katana = katana;
             this.armor = armor;
+            this.punch = punch;
         }
     }
-    const character_01 = new Hero('Jin Sakai','Samurai','60','Sakai Katana','Broken armor');
+    const character_01 = new Hero('Jin Sakai','Samurai','60','Sakai Katana','Broken armor', '35');
     character_01.stats();
 
     // Line 5: The 'class' and 'extends' operators define a new object importing the function from the index file.
@@ -29,23 +31,28 @@
     // Line 23: Execution of line 23 with the 'function' from (connection between) 'index.js'.
     
     class Enemy extends GhostOfTsushima {
-        constructor(enemy,mongol,life,blade,armor) {
+        constructor(enemy,mongol,life,blade,armor,punch) {
             const name = enemy + ' from Mongolia';
             const type = mongol + ' intruder';
             const health = life + ' hit points';
             const weapon = blade + ' weapons';
             const defense = armor + ' for elite warriors';
+            const hit = punch + ' counter attack';
 
-            super(name,type,health,weapon,defense);
+            super(name,type,health,weapon,defense,hit);
 
             this.enemy = enemy;
             this.mongol = mongol;
             this.life = life;
             this.blade = blade;
             this.armor = armor;
+            this.punch = punch;
         }
     }
     const character_02 = new Enemy('Mongol Warlord','soldier','80','shield with a sword','Gold Plated armor');
     character_02.stats();
 
-    // TODO: How many module.exports are available to use in one file?
+    module.exports = Hero;
+    module.exports = Enemy;
+    // TODO: Is it allowed to have more than one 'module.exports' instruction per line?
+    // TODO: How many 'extends' are available in every 'class'?
